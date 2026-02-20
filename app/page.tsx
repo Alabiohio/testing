7853db44a6037d3a79e0a37f74de13c7751d3884
Alabiohio <your@email.com>
2026-02-20 23:08:51 +0100

    comment

--- app/page.tsx
+++ app/page.tsx
@@ -269,7 +269,7 @@ export default function Home() {
                   fill
                   className="object-cover group-hover:scale-110 transition-transform duration-700"
                 />
-                <div className={`absolute inset-0 bg-gradient-to-t ${cat.color} to-transparent opacity-60`} />
+               {/* <div className={`absolute inset-0 bg-gradient-to-t ${cat.color} to-transparent opacity-60`} /> */}
               </div>
               <div className="p-8">
                 <h3 className="text-2xl font-black text-deep-green dark:text-white mb-3 uppercase tracking-tight">{cat.name}</h3>
